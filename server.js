2e7bc42e6ef13ed9fdc56565838d8261d95bf0d6
Yuichi Kanno <kanno_yuuichi@waku-2.com>
2023-11-20 18:49:26 +0900

    server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res, next) => {
+  res.end("Top Page");
+});
+
+app.listen(port, () => {
+  console.log(`Serverが起動しました。portは${port}番です。`);
+});
