a140ff5447fa86ebe9bf9b9fd774b86720d797a1
Yuichi Kanno <fksmhr077@gmail.com>
2023-12-17 14:02:46 +0900

    xss csp nonce strict-dynamic

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
+const crypto = require("crypto");
 const express = require("express");
 const api = require("./routes/api");
 
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 app.use("/api", api);
 
 app.get("/", (req, res, next) => {
-  res.end("Top Page");
+	res.end("Top Page");
+});
+
+app.get("/csp", (req, res, next) => {
+	const nonceValue = crypto.randomBytes(16).toString("base64");
+
+	res.header(
+		"Content-Security-Policy",
+		`script-src 'nonce-${nonceValue}' 'strict-dynamic';` +
+			"object-src 'none';" +
+			"base-uri 'none;'"
+	);
+	res.render("csp", { nonce: nonceValue });
 });
 
 app.listen(port, () => {
-  console.log(`Serverが起動しました。portは${port}番です。`);
+	console.log(`Serverが起動しました。portは${port}番です。`);
 });
