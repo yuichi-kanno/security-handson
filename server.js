995ef0e8d0e3e45063c88fc89a3fec7b19e47524
Yuichi Kanno <kanno_yuuichi@waku-2.com>
2024-01-24 14:43:15 +0900

    CSRF 検証用ページ、罠ページ

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const crypto = require("crypto");
 const express = require("express");
 const api = require("./routes/api");
+const csrf = require("./routes/csrf");
 
 const app = express();
 const port = 3000;
@@ -9,23 +10,24 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use("/api", api);
+app.use("/csrf", csrf);
 
 app.get("/", (req, res, next) => {
-	res.end("Top Page");
+  res.end("Top Page");
 });
 
 app.get("/csp", (req, res, next) => {
-	const nonceValue = crypto.randomBytes(16).toString("base64");
+  const nonceValue = crypto.randomBytes(16).toString("base64");
 
-	res.header(
-		"Content-Security-Policy",
-		`script-src 'nonce-${nonceValue}' 'strict-dynamic';` +
-			"object-src 'none';" +
-			"base-uri 'none;'"
-	);
-	res.render("csp", { nonce: nonceValue });
+  res.header(
+    "Content-Security-Policy",
+    `script-src 'nonce-${nonceValue}' 'strict-dynamic';` +
+      "object-src 'none';" +
+      "base-uri 'none;'"
+  );
+  res.render("csp", { nonce: nonceValue });
 });
 
 app.listen(port, () => {
-	console.log(`Serverが起動しました。portは${port}番です。`);
+  console.log(`Serverが起動しました。portは${port}番です。`);
 });
